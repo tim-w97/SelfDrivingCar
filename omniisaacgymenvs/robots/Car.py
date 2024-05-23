24b987fc6bd26f9f87d7978655a8e3f048a45cdb
Tim <timwagner997@gmail.com>
2024-05-23 15:42:50 +0200

    added Car class

---
+++ omniisaacgymenvs/robots/Car.py
@@ -0,0 +1,28 @@
+class Car(Robot):
+    def __init__(
+        self,
+        prim_path: str,
+        name: Optional[str] = "Car",
+        usd_path: Optional[str] = None,
+        translation: Optional[np.ndarray] = None,
+        orientation: Optional[np.ndarray] = None,
+    ) -> None:
+
+        self._usd_path = usd_path
+        self._name = name
+
+        if self._usd_path is None:
+            assets_root_path = get_assets_root_path()
+            if assets_root_path is None:
+                carb.log_error("Could not find Isaac Sim assets folder")
+            self._usd_path = assets_root_path + "/Isaac/Robots/Car/car.usd"
+
+        add_reference_to_stage(self._usd_path, prim_path)
+
+        super().__init__(
+            prim_path=prim_path,
+            name=name,
+            translation=translation,
+            orientation=orientation,
+            articulation_controller=None,
+        )
