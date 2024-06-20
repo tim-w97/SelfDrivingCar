d6a00546ea9e34f2eb505d13c68f3cb0699715e3
ndemel <ndemel@FB-LS006-01.hof-university.de>
2024-06-20 14:19:40 +0200

    changed path

--- omniisaacgymenvs/tasks/car.py
+++ omniisaacgymenvs/tasks/car.py
@@ -60,7 +60,7 @@ class CarTask(RLTask):
         print("get_car() prim_path", prim_path)
 
         car = Car(
-            usd_path="/home/HOF-UNIVERSITY.DE/ndemel/SelfDrivingCar/assets/car_with_wheels.usd", #/home/HOF-UNIVERSITY.DE/ndemel/SelfDrivingCar/assets/car.usd
+            usd_path="assets/car_with_wheels.usd", #/home/HOF-UNIVERSITY.DE/ndemel/SelfDrivingCar/assets/car.usd
             prim_path=prim_path, name="Car",
             translation=self._car_positions
         )
